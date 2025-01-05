335cfc58ebf99703c60604acd2531ceb56aa9cca
Shikhar Basnet <143075901+Shikhar-Basnet@users.noreply.github.com>
2025-01-05 20:29:26 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
-
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    useEffect(() => {
+        // Check if user is authenticated
+        axios.get('http://localhost:5000')
+            .then((res) => {
+                if (res.data.Status === 'Success') {
+                    setIsAuthenticated(true);
+                } else {
+                    setIsAuthenticated(false);
+                }
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                {/* Protected routes */}
+                <Route
+                    path="/admin-dashboard"
+                    element={
+                        <ProtectedRoute isAuthenticated={isAuthenticated}>
+                            <AdminDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/user-dashboard"
+                    element={
+                        <ProtectedRoute isAuthenticated={isAuthenticated}>
+                            <UserDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;

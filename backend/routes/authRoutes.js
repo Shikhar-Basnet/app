294bfbc3e58aef1d9d840c7e5b9d3b29b8cbdff6
Shikhar Basnet <143075901+Shikhar-Basnet@users.noreply.github.com>
2025-01-16 17:04:07 +0545

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, login, logout } from '../controllers/authController.js';
+import { register, login, checkAuth, logout } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/check-auth', checkAuth);
 router.get('/logout', logout);
 
 export default router;
